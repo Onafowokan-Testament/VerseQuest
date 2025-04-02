0eab509b4fcb3f9b4f5e7d2b3a8c97368bf185a3
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2025-04-02 14:38:13 +0100

    create table

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, ForeignKey, Integer, String,Boolean, relationship
-
-from .database import Base
+from database import Base
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 
 class Question(Base):
@@ -14,22 +14,21 @@ class Question(Base):
     option_d = Column(String)
     correct_answer = Column(String)
 
+    user_results = relationship("UserResult", back_populates="question")
+
     def __repr__(self):
         return f"<Question(id={self.id}, chapter={self.chapter}, question_text={self.question_text})>"
 
 
 class UserResult(Base):
-
     __tablename__ = "user_results"
-
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, index=True)
-    question_id = Column(Integer, ForeignKey("question.id"))
+    question_id = Column(Integer, ForeignKey("questions.id"))
     user_answer = Column(String)
     is_correct = Column(Boolean)
 
-
-    question = relationship("Question", back_populates = "user_results")
+    question = relationship("Question", back_populates="user_results")
 
     def __repr__(self):
         return f"<UserResult(id={self.id}, user_id={self.user_id}, question_id={self.question_id})>"
